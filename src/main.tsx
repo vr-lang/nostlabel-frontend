07b3a2ddf7a8487f21bb9edffc4713a37ea617d5
Yuvraj Singh <yuvrajsingh@Yuvrajs-MacBook-Air.local>
2026-06-11 11:22:02 +0530

    Loader added and frontend fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import { StartupLoadingProvider } from './context/StartupLoadingContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <StartupLoadingProvider>
-        <App />
-      </StartupLoadingProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
